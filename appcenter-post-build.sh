594f6cfecbd73bded0554b7fa4b222d2bd99eff6
Hanif Nur Amrullah <amrews@hotmail.com>
2018-06-24 16:49:47 +0700

    update header title

---
+++ appcenter-post-build.sh
@@ -0,0 +1,8 @@
+export $(egrep -v '^#' .env | xargs)
+
+if [ "$APPCENTER_BRANCH" == "master" ];
+then
+    appcenter codepush release-react -a Sunnah-Studio/Sunnah-Radio-Android -d Staging -t APP_VERSION_NAME
+else
+    echo "Current branch is $APPCENTER_BRANCH"
+fi
